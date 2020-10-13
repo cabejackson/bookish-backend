b1c76de94c37c2f220be708c95e361ab4271a0be
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2020-10-13 16:57:25 -0400

    deploy1

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://dunder_mifflin@localhost/bnb-tests",
-  // TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/bnb-test',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/bnb-tests',
+  // DB_URL: process.env.DB_URL || "postgresql://dunder_mifflin@localhost/bnb-tests",
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/bnb-test',
   JWT_SECRET: process.env.JWT_SECRET || 'october'
 };
